b822ea6971b7105614d87a8b41f69ce5f343442b
Yunfei Zhang <beethovenzhang@gmail.com>
2019-01-29 20:55:30 -0800

    Optimized the layout of the webpage

--- app.py
+++ app.py
@@ -653,4 +653,4 @@ def categories_json():
 
 if __name__ == "__main__":
     app.secret_key = 'super_secret_key'
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded = True)
