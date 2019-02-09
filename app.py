e72aa7b183236e41dc6fade8b0199e9f66a1f2d8
Yunfei Zhang <beethovenzhang@gmail.com>
2019-02-09 14:07:17 -0800

    Change ip and port to the deployment environment

--- app.py
+++ app.py
@@ -556,4 +556,7 @@ def categories_json():
 
 if __name__ == "__main__":
     app.secret_key = 'super_secret_key'
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded = True)
+    try:
+        app.run(host="34.221.214.78", port=80, debug=True, threaded = True)
+    except:
+        app.run(host="0.0.0.0", port=5000, debug=True, threaded = True)
